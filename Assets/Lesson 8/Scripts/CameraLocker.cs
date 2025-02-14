801ecb5f3b09a17e37dcc0141ca45a84cfc249ba
den111matliak <veselkostrug@gmail.com>
2025-02-14 07:51:12 +0200

    [ADD] New Scene (Lesson 8)
    Added new scene, terrain, trees, village, lighthouse and capsule with controls

---
+++ Assets/Lesson 8/Scripts/CameraLocker.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace Lesson8
+{
+    public class CameraLocker : MonoBehaviour
+    {
+        [SerializeField] private CameraController _cameraController;
+        [SerializeField] private Transform _regularYawAnchor;
+        [SerializeField] private Transform _lockedYawAnchor;
+
+        private void Start()
+        {
+            InputController.OnCameraLock += CameraLockHandler;
+        }
+
+        public void Lock(bool isLocked)
+        {
+            _cameraController.SetYawAnchor(isLocked ? _lockedYawAnchor : _regularYawAnchor);
+        }
+
+        private void CameraLockHandler(bool isLocked)
+        {
+            Lock(isLocked);
+        }
+    }
+}
