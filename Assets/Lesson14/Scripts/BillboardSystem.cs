cb9a8b1fbbd6e32beb9b6c9d125fda851683d95a
den111matliak <veselkostrug@gmail.com>
2025-03-13 07:51:22 +0200

    Lesson14
    save before applying shooting

---
+++ Assets/Lesson14/Scripts/BillboardSystem.cs
@@ -0,0 +1,30 @@
+#if UNITY_EDITOR
+using Lesson14;
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace Lesson14
+{
+    public class BillBoardSystem : MonoBehaviour
+    {
+        [SerializeField] protected CameraSystem _cameraSystem;
+
+        [SerializeField] protected BillboardBase[] _horizontalBillboards;
+
+        [ContextMenu("Find Billboards")]
+        private void FindBillboards()
+        {
+            _horizontalBillboards = FindObjectsOfType<BillboardBase>(true);
+#if UNITY_EDITOR
+            EditorUtility.SetDirty(this);
+#endif
+        }
+
+        protected virtual void Awake()
+        {
+            for (int i = 0; i < _horizontalBillboards.Length; ++i)
+                _horizontalBillboards[i].SetCamera(_cameraSystem.Camera);
+        }
+    }
+}
---
+++ Assets/Lesson14/Scripts/CameraSystem.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Lesson14
+{
+    public class CameraSystem : MonoBehaviour
+    {
+        [SerializeField] private Camera _camera;
+
+        public Camera Camera => _camera;
+    }
+}
